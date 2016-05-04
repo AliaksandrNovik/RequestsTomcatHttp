e251734707a86aa76062ef62e99121507dd20350
Пользователь <>
2016-05-04 22:32:56 +0300

    added php file

--- php/test.php
+++ php/test.php
@@ -1,9 +1,9 @@
 
  <?php
- define('HOST','tomcat7-aliaksandrnovik.rhcloud.com');
- define('USER','admin6PK4xdu');
- define('PASS','w6WTVFDyPxGU');
- define('DB','message');
+ define('HOST','http://146.185.164.93');
+ define('USER','root');
+ define('PASS','7Ea4fae339');
+ define('DB','tomcat7');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 
